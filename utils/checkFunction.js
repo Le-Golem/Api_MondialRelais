047027009f9f657ba87bc33187ac937d23347940
unknown <laethemalexis@gmail.com>
2023-06-08 13:43:19 +0200

    changement dans checkfunction

--- utils/checkFunction.js
+++ utils/checkFunction.js
@@ -4,7 +4,7 @@ function checkData(data){
 
     const schema = Joi.object({
         Pays: Joi.string().min(2).max(2).required(),
-        Ville: Joi.string().min(5).max(100).required(),
+        Ville: Joi.string().min(5).max(100).allow(null, ''),
         CP: Joi.string().min(5).max(5).required(),
         Latitude: Joi.string().allow(null, ''),
         Longitude: Joi.string().allow(null, '')
